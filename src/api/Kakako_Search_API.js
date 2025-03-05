c5397e4e398ffe3aa134f3fd69f44cfa8906a1fa
Hyung <h9564289@gmail.com>
2025-03-05 22:38:28 +0900

    오타 수정

--- src/api/Kakako_Search_API.js
+++ src/api/Kakako_Search_API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const KakaoAPI = async (query) => {
     try {
-        const response = await axios.get(process.env.REACT_APP_PUBLIC_URL,{
+        const response = await axios.get('http://localhost:5000/api/search', {
             params: { query }
         });
 
--- src/api/Kakao_Login_api.js
+++ src/api/Kakao_Login_api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getAccessToken = (code) => {
-  return axios.get(process.env.REACT_APP_PUBLIC_URL, {
+  return axios.get(`http://localhost:5000/auth/Kakao`, {
     params: { code: code },
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
   });
