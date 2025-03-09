da5aeb687f3a6238195ba01b2c1fe788788e6f5b
Hyung <h9564289@gmail.com>
2025-03-09 12:45:38 +0900

    깃 충돌 수정

--- src/api/Kakao_Address_api.js
+++ src/api/Kakao_Address_api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-const KaKaoAPI = process.env.REACT_APP_KAKAO_REST_API_KEY;
-=======
 const KaKaoAPI = '6116026697d7c84da46212493aef754b';
->>>>>>> c97f007 (서버 도메인 설정)
 const apiUrl = 'https://dapi.kakao.com/v2/local/geo/coord2address.json';
 
 export const AddressCoordinates = async (latitude, longitude) => {
