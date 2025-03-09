88491b032fe6c23aa0df0951cdca7923920e826a
Hyung <h9564289@gmail.com>
2025-03-09 13:09:37 +0900

    REDIRECT_URI env 수정

--- src/hooks/useKakaoLogin.jsx
+++ src/hooks/useKakaoLogin.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import useAuthStore from '../store/KakaoAuthStore';
 
 const KAKAO_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
-const REDIRECT_URI = 'https://restaurant-search-eight.vercel.app/Login';
+const REDIRECT_URI = process.env.REACT_APP_KAKAO_LOGIN_REDIRECT_URI;
 
 const useKakaoLogin = () => {
   const navigate = useNavigate();
