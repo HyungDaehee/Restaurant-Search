5efdcbb8d13bce563416650756640e6cb03da30d
Hyung <h9564289@gmail.com>
2025-03-10 23:38:52 +0900

    오타 수정 및 카카오  리다이렉트 재설정

--- src/api/Kakako_Search_API.js
+++ src/api/Kakako_Search_API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const KakaoAPI = async (query) => {
     try {
-        const response = await axios.get('https://express-server-olive.vercel.app//api/search', {
+        const response = await axios.get('https://express-server-olive.vercel.app/api/search', {
             params: { query }
         });
 
